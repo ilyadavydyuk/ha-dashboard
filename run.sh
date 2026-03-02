754689bd45131039736063077ef815d87327bacd
Ilya Davydyuk <id@kalinka-realty.com>
2026-03-02 18:44:23 +0300

    fix nginx

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo "Starting HA Dashboard..."
 
-# Запускаем Next.js в фоне
 node /app/server.js &
 
-# Ждём пока Next.js поднимется
 sleep 3
 
-# Запускаем nginx
+# Проверяем конфиг nginx перед запуском
+nginx -t
+
 nginx -g "daemon off;"
