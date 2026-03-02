7f4df65bc613457a8f2ef3dd47bf80ea7d7853ac
Ilya Davydyuk <id@kalinka-realty.com>
2026-03-02 18:53:28 +0300

    fix nginx

--- run.sh
+++ run.sh
@@ -5,5 +5,8 @@ node /app/server.js &
 
 sleep 3
 
+# Проверяем доступен ли Next.js
+wget -q -O- http://127.0.0.1:3000 > /dev/null && echo "Next.js OK" || echo "Next.js NOT reachable"
+
 nginx -t
 nginx -g "daemon off;" || cat /tmp/nginx_error.log
